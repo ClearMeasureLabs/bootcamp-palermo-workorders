e409449c431ca90ef104c8f9da72cba41443a94d
Joey Wills <Joey.Wills@biberk.com>
2025-08-04 13:37:18 -0400

    Added shelve btn and test.

---
+++ src/AcceptanceTests/WorkOrders/WorkOrderShelveTests.cs
@@ -0,0 +1,31 @@
+﻿using ClearMeasure.Bootcamp.Core.Model.StateCommands;
+using ClearMeasure.Bootcamp.UI.Shared.Pages;
+
+namespace ClearMeasure.Bootcamp.AcceptanceTests.WorkOrders;
+
+public class WorkOrderShelveTests : AcceptanceTestBase
+{
+    [Test]
+    public async Task ShouldShelveAndRevertToAssigned()
+    {
+        await LoginAsCurrentUser();
+
+        var order = await CreateAndSaveNewWorkOrder();
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        order = await AssignExistingWorkOrder(order, CurrentUser.UserName);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        await Click(nameof(WorkOrderManage.Elements.CommandButton) +
+                    AssignedToInProgressCommand.Name);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        await Click(nameof(WorkOrderManage.Elements.CommandButton) +
+                    InProgressToAssignedCommand.Name);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        await Expect(Page.GetByTestId(nameof(WorkOrderManage.Elements.Status)))
+            .ToHaveTextAsync(WorkOrderStatus.Assigned.FriendlyName);
+
+    }
+}
---
+++ src/Core/Model/StateCommands/InProgressToAssignedCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace ClearMeasure.Bootcamp.Core.Model.StateCommands;
+public record InProgressToAssignedCommand(WorkOrder WorkOrder, Employee CurrentUser) : StateCommandBase(WorkOrder, CurrentUser)
+{
+    public static string Name { get; set; } = "Shelve";
+
+    public override WorkOrderStatus GetBeginStatus()
+    {
+        return WorkOrderStatus.InProgress;
+    }
+
+    public override WorkOrderStatus GetEndStatus()
+    {
+        return WorkOrderStatus.Assigned;
+    }
+
+    protected override bool UserCanExecute(Employee currentUser)
+    {
+        return currentUser == WorkOrder.Assignee;
+    }
+
+    public override string TransitionVerbPresentTense => Name;
+    public override string TransitionVerbPastTense => "Shelved";
+}
