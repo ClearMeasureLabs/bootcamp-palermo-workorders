707c2e63c98e82bec914b6f6f86861b36b571ac9
Sam Stack <sam.stack@multitracks.com>
2025-08-04 16:48:26 -0400

    In progress to cancelled shell

---
+++ src/Core/Model/StateCommands/InProgressToCancelledCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearMeasure.Bootcamp.Core.Model.StateCommands;
+public record InProgressToCancelledCommand(WorkOrder WorkOrder, Employee CurrentUser) : StateCommandBase(WorkOrder, CurrentUser)
+{
+	public override string TransitionVerbPresentTense => "Cancel";
+
+	public override string TransitionVerbPastTense => "Cancelled";
+
+	public override WorkOrderStatus GetBeginStatus()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override WorkOrderStatus GetEndStatus()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override bool UserCanExecute(Employee currentUser)
+	{
+		throw new NotImplementedException();
+	}
+}
