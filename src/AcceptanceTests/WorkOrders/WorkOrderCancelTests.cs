0f8f496e309edbc5ba073e1e32dfe87b3fc75dd8
Jeffrey Palermo <jeffrey@clear-measure.com>
2025-08-04 11:30:22 -0500

    Added the cancel button and the functionality is working

---
+++ src/AcceptanceTests/WorkOrders/WorkOrderCancelTests.cs
@@ -0,0 +1,27 @@
+﻿using ClearMeasure.Bootcamp.Core.Model.StateCommands;
+using ClearMeasure.Bootcamp.UI.Shared.Pages;
+
+namespace ClearMeasure.Bootcamp.AcceptanceTests.WorkOrders;
+
+public class WorkOrderCancelTests : AcceptanceTestBase
+{
+    [Test]
+    public async Task ShouldAssignAndCancel()
+    {
+        await LoginAsCurrentUser();
+
+        var order = await CreateAndSaveNewWorkOrder();
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        order = await AssignExistingWorkOrder(order, CurrentUser.UserName);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        await Click(nameof(WorkOrderManage.Elements.CommandButton) + 
+                    CancelledToDraftCommand.Name);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        await Expect(Page.GetByTestId(nameof(WorkOrderManage.Elements.Status)))
+            .ToHaveTextAsync(WorkOrderStatus.Draft.FriendlyName);
+
+    }
+}
