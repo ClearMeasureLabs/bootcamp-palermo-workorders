2bcd1f02047f4b4b8c1d3cb2052ae2c7e475a24a
India <india.lattimore@biberk.com>
2025-08-04 17:10:19 -0500

    work chat unit test

---
+++ src/AcceptanceTests/WorkOrders/WorkOrderChatTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using ClearMeasure.Bootcamp.Core.Model.StateCommands;
+using ClearMeasure.Bootcamp.UI.Shared;
+using ClearMeasure.Bootcamp.UI.Shared.Pages;
+using System.Text.RegularExpressions;
+using ClearMeasure.Bootcamp.Core.Queries;
+
+namespace ClearMeasure.Bootcamp.AcceptanceTests.WorkOrders;
+
+public class WorkOrderSaveChatTests : AcceptanceTestBase
+{
+    [Test]
+    public async Task ShouldAppearOnWorkOrderScreens()
+    {
+        await LoginAsCurrentUser();
+
+        WorkOrder order = await CreateAndSaveNewWorkOrder();
+
+        await ClickWorkOrderNumberFromSearchPage(order);
+        order = await AssignExistingWorkOrder(order, CurrentUser.UserName);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        //test code india
+        var test = Page.GetByTestId("work-order-chat");
+        var test1 = Page.GetByText("Ask the AI assistant about this work order...");
+
+        await Expect(Page.GetByText("Ask the AI assistant about this work order..."))
+            .ToBeVisibleAsync();
+
+        Console.WriteLine(order);
+    }
+}
