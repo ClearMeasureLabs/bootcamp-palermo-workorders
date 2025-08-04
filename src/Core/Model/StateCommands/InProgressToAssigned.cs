370d63f3f8d8cb411b6f4a7b3319a22a67bd189e
Joe Arndt <Joe.Arndt@biberk.com>
2025-08-04 12:05:06 -0500

    wip

---
+++ src/Core/Model/StateCommands/InProgressToAssigned.cs
@@ -0,0 +1,25 @@
+﻿namespace ClearMeasure.Bootcamp.Core.Model.StateCommands;
+
+public record InProgressToAssigned(WorkOrder WorkOrder, Employee CurrentUser) : StateCommandBase(WorkOrder, CurrentUser)
+
+{
+    public static string Name { get; set; } = "Shelve";
+
+    public override WorkOrderStatus GetBeginStatus()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override WorkOrderStatus GetEndStatus()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override bool UserCanExecute(Employee currentUser)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string TransitionVerbPresentTense { get; }
+    public override string TransitionVerbPastTense { get; }
+}
