5a533d8b1f9ded02b01595ca216db5f475ccd4f0
Tom Opgenorth <tom.opgenorth@clear-measure.com>
2025-11-14 06:57:00 -0700

    Code formatting.

--- src/Database/Program.cs
+++ src/Database/Program.cs
@@ -1,9 +1,9 @@
-﻿using DbUp;
+﻿using System.Diagnostics;
+using DbUp;
+using DbUp.Engine;
 using DbUp.Helpers;
-using DbUp.ScriptProviders;
 using DbUp.Support;
 using Microsoft.Data.SqlClient;
-
 
 int Fail(string message, int code = -1)
 {
@@ -13,51 +13,8 @@ int Fail(string message, int code = -1)
     return code;
 }
 
-int CheckDockerContainer(string containerName)
-{
-    try
-    {
-        var processStartInfo = new System.Diagnostics.ProcessStartInfo
-        {
-            FileName = "docker",
-            Arguments = "ps --format \"{{.Names}}\"",
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
 
-        using var process = System.Diagnostics.Process.Start(processStartInfo);
-        if (process == null)
-        {
-            return Fail("Failed to start docker process. Is Docker installed?");
-        }
-
-        var output = process.StandardOutput.ReadToEnd();
-        var error = process.StandardError.ReadToEnd();
-        process.WaitForExit();
-
-        if (process.ExitCode != 0)
-        {
-            return Fail($"Docker is not accessible. Error: {error}");
-        }
-
-        var containerNames = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-        if (!containerNames.Contains(containerName))
-        {
-            return Fail($"Docker container '{containerName}' is not running. Available containers: {string.Join(", ", containerNames)}");
-        }
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Found Docker container: {containerName}");
-        Console.ResetColor();
-        return 0;
-    }
-    catch (Exception ex)
-    {
-        return Fail($"Error checking Docker: {ex.Message}");
-    }
-}
+#region Read command line parameters
 
 // Takes 4 arguments, just like AliaASql
 // <ALIASQL ACTION> <SERVER NAME> <DATABASE NAME> <SCRIPT DIRECTORY>
@@ -77,12 +34,67 @@ var databaseName = args[2];
 // Normalize path separators to be platform-appropriate
 var scriptDir = args[3].Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
 
+#endregion
+
+#region Check for Docker
+
 // Check if Docker is running and container exists
+int CheckDockerContainer(string containerName)
+{
+    try
+    {
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "docker",
+            Arguments = "ps --format \"{{.Names}}\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using var process = Process.Start(processStartInfo);
+        if (process == null)
+        {
+            return Fail("Failed to start docker process. Is Docker installed?");
+        }
+
+        var output = process.StandardOutput.ReadToEnd();
+        var error = process.StandardError.ReadToEnd();
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            return Fail($"Docker is not accessible. Error: {error}");
+        }
+
+        var containerNames = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        if (!containerNames.Contains(containerName))
+        {
+            return Fail(
+                $"Docker container '{containerName}' is not running. Available containers: {string.Join(", ", containerNames)}");
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Found Docker container: {containerName}");
+        Console.ResetColor();
+        return 0;
+    }
+    catch (Exception ex)
+    {
+        return Fail($"Error checking Docker: {ex.Message}");
+    }
+}
+
 var dockerCheckResult = CheckDockerContainer($"sql2022-bootcamp-tests-{databaseName}");
 if (dockerCheckResult != 0)
 {
     return dockerCheckResult;
 }
+
+#endregion
+
+#region Build connection string
 
 // TODO [TO20251111] Should use the environment variable connection if available. Note that we need TrustServerCertificate=True for local dev with self-signed certs.
 // TODO [TO20251111] Note the hardcoded username.
@@ -97,8 +109,10 @@ var builder = new SqlConnectionStringBuilder
     Encrypt = false
 };
 var connectionString = builder.ConnectionString;
-
 EnsureDatabase.For.SqlDatabase(connectionString);
+
+#endregion
+
 
 // 1) RunOnce scripts: Create + Update (journaled)
 var runOnce = DeployChanges.To
@@ -117,8 +131,8 @@ if (!runOnceResult.Successful)
 // 2) RunAlways scripts: things to re-apply each run (procs/views/perms)
 var runAlways = DeployChanges.To
     .SqlDatabase(connectionString)
-    .WithScriptsFromFileSystem(path:Path.Join(scriptDir, "Everytime"), 
-        sqlScriptOptions:new DbUp.Engine.SqlScriptOptions { ScriptType = ScriptType.RunAlways })
+    .WithScriptsFromFileSystem(Path.Join(scriptDir, "Everytime"),
+        new SqlScriptOptions { ScriptType = ScriptType.RunAlways })
     .JournalTo(new NullJournal())
     .LogToConsole()
     .Build();
