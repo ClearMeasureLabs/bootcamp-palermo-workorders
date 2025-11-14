11776c85c82be322a25c73530a2753a63fcfa966
Tom Opgenorth <tom.opgenorth@clear-measure.com>
2025-11-14 13:52:00 -0700

    Fix/update the scripts for the AzurePipeline.

--- src/Database/scripts/UpdateAzurePipelineSql.ps1
+++ src/Database/scripts/UpdateAzurePipelineSql.ps1
@@ -9,6 +9,7 @@ param(
 Write-Output "Recursive directory listing for diagnostics"
 Get-ChildItem -Recurse
 
+$bootcampdatabaseDll = Join-Path ".\scripts" "ClearMeasure.Bootcamp.Database.dll"
 Write-Host "Executing dotnet .\scripts\ClearMeasure.Bootcamp.Database.dll $DatabaseAction $DatabaseServer $DatabaseName .\scripts"
 
 dotnet .\scripts\ClearMeasure.Bootcamp.Database.dll $DatabaseAction $DatabaseServer $DatabaseName .\scripts
--- src/Database/scripts/UpdateAzureSql.ps1
+++ src/Database/scripts/UpdateAzureSql.ps1
@@ -9,8 +9,11 @@ $DatabasePassword = $OctopusParameters["DatabasePassword"]
 Write-Output "Recursive directory listing for diagnostics"
 Get-ChildItem -Recurse
 
+
+$bootcampdatabaseDll = Join-Path ".\scripts" "ClearMeasure.Bootcamp.Database.dll"
+
 Write-Host "Executing dotnet .\scripts\ClearMeasure.Bootcamp.Database.dll $DatabaseAction $DatabaseServer $DatabaseName .\scripts"
-dotnet .\scripts\ClearMeasure.Bootcamp.Database.dll $DatabaseAction $DatabaseServer $DatabaseName .\scripts
+dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName .\scripts
 if ($lastexitcode -ne 0) {
     throw ("Database migration had an error.")
 }
