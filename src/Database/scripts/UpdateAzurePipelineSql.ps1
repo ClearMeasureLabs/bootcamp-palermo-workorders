16019685f23b6756b3ebdf5509ddfda8d77c0583
Tom Opgenorth <tom.opgenorth@clear-measure.com>
2025-11-14 22:05:44 -0700

    Need to provide the database user and & pwd in UpdateAzurePipelineSql.ps1.

--- src/Database/scripts/UpdateAzurePipelineSql.ps1
+++ src/Database/scripts/UpdateAzurePipelineSql.ps1
@@ -11,7 +11,7 @@ $bootcampdatabaseDll = Join-Path $PWD "ClearMeasure.Bootcamp.Database.dll"
 
 Write-Host "Executing dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName $scriptDir"
 
-dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName $scriptDir	
+dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword
 
 if ($lastexitcode -ne 0) {
     throw ("Database migration had an error.")
