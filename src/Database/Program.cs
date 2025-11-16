baf93c09800efef08c61de374fe7443e3a7641d9
Tom Opgenorth <tom.opgenorth@clear-measure.com>
2025-11-16 09:21:00 -0700

    Only perform the one DB action (for now).

--- src/Database/Program.cs
+++ src/Database/Program.cs
@@ -1,4 +1,5 @@
-﻿using ClearMeasure.Bootcamp.Database.Console;
+﻿using System.Runtime.InteropServices.JavaScript;
+using ClearMeasure.Bootcamp.Database.Console;
 using Spectre.Console.Cli;
 
 var app = new CommandApp();
@@ -6,14 +7,15 @@ app.Configure(config =>
 {
     config.SetApplicationName("ChurchBulletin.Database");
     config.CaseSensitivity(CaseSensitivity.None); // Command names are case-insensitive
-    
+
+    // TODO [TO20251116] - Need to map the AliaSQL actions to DbUp commands. For now everything is a "Rebuild".
     config.AddCommand<BaselineDatabaseCommand>("baseline")
         .WithDescription("Mark all existing scripts as executed without running them (for existing databases)");
     
     config.AddCommand<RebuildDatabaseCommand>("rebuild")
         .WithDescription("Rebuild the database by running Create, Update, Everytime, and TestData scripts");
-    
-    config.AddCommand<UpdateDatabaseCommand>("update")
+
+    config.AddCommand<RebuildDatabaseCommand>("update")
         .WithDescription("Update the database by running only the Update scripts");
 });
 
