0830fad97e7572c6e0ab13812f3e1cffaa2ff6ad
Tom Opgenorth <tom.opgenorth@clear-measure.com>
2025-11-16 19:19:05 -0700

    Just renaming some variables.

--- src/Database/scripts/UpdateAzurePipelineSql.ps1
+++ src/Database/scripts/UpdateAzurePipelineSql.ps1
@@ -6,26 +6,25 @@ param(
 	[string]$DatabasePassword
 )
 
-
 # Find ClearMeasure.Bootcamp.Database.dll in current folder or subfolders
-$bootcampdatabaseDll = Get-ChildItem -Path $PWD -Filter "ClearMeasure.Bootcamp.Database.dll" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+$databaseAssembly = Get-ChildItem -Path $PWD -Filter "ClearMeasure.Bootcamp.Database.dll" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
 
-if (-not $bootcampdatabaseDll) {
+if (-not $databaseAssembly) {
     throw "Could not find ClearMeasure.Bootcamp.Database.dll in $PWD or its subfolders"
 }
 
-Write-Host "Found database DLL at: $bootcampdatabaseDll"
+Write-Host "Found database DLL at: $databaseAssembly"
 $scriptDir = Resolve-Path -Path (".\scripts")
-Write-Host "Executing dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser <REDACTED>"
+Write-Host "Executing dotnet $databaseAssembly $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser <REDACTED>"
 
 # This is a one-off - we need to baseline the database the first time so that DbUp doesn't try to re-apply all the scripts
 # in an existing database.
-dotnet $bootcampdatabaseDll baseline $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword 
+dotnet $databaseAssembly baseline $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword 
 if ($lastexitcode -ne 0) {
     throw ("Failed to baseline the database.")
 }
 
-dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword
+dotnet $databaseAssembly $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword
 if ($lastexitcode -ne 0) {
     throw ("Database migration had an error.")
 }
