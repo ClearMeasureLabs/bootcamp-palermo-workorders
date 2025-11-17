d673ae74bde0ce28a66191fb9751bc0e629b8001
Tom Opgenorth <tom.opgenorth@clear-measure.com>
2025-11-16 19:32:29 -0700

    Baselining done - get rid of it.

--- src/Database/scripts/UpdateAzurePipelineSql.ps1
+++ src/Database/scripts/UpdateAzurePipelineSql.ps1
@@ -6,24 +6,14 @@ param(
 	[string]$DatabasePassword
 )
 
-# Find ClearMeasure.Bootcamp.Database.dll in current folder or subfolders
 $databaseAssembly = Get-ChildItem -Path $PWD -Filter "ClearMeasure.Bootcamp.Database.dll" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+$scriptDir = Resolve-Path -Path (".\scripts")
 
 if (-not $databaseAssembly) {
     throw "Could not find ClearMeasure.Bootcamp.Database.dll in $PWD or its subfolders"
 }
 
-Write-Host "Found database DLL at: $databaseAssembly"
-$scriptDir = Resolve-Path -Path (".\scripts")
 Write-Host "Executing dotnet $databaseAssembly $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser <REDACTED>"
-
-# This is a one-off - we need to baseline the database the first time so that DbUp doesn't try to re-apply all the scripts
-# in an existing database.
-dotnet $databaseAssembly baseline $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword 
-if ($lastexitcode -ne 0) {
-    throw ("Failed to baseline the database.")
-}
-
 dotnet $databaseAssembly $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword
 if ($lastexitcode -ne 0) {
     throw ("Database migration had an error.")
--- src/Database/scripts/UpdateAzureSql.ps1
+++ src/Database/scripts/UpdateAzureSql.ps1
@@ -1,6 +1,3 @@
-#
-# UpdateAzureSQL.ps1
-#
 $DatabaseServer = $OctopusParameters["DatabaseServer"]
 $DatabaseName = $OctopusParameters["DatabaseName"]
 $DatabaseAction = $OctopusParameters["DatabaseAction"]
@@ -8,11 +5,16 @@ $DatabaseUser = $OctopusParameters["DatabaseUser"]
 $DatabasePassword = $OctopusParameters["DatabasePassword"]
 
 
-$scriptDir = Join-Path $PWD "scripts"
-$bootcampdatabaseDll = Join-Path $PWD "ClearMeasure.Bootcamp.Database.dll"
 
-Write-Host "Executing dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName $scriptDir"
-dotnet $bootcampdatabaseDll $DatabaseAction $DatabaseServer $DatabaseName $scriptDir
+$databaseAssembly = Get-ChildItem -Path $PWD -Filter "ClearMeasure.Bootcamp.Database.dll" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty FullName
+$scriptDir = Resolve-Path -Path (".\scripts")
+
+if (-not $databaseAssembly) {
+    throw "Could not find ClearMeasure.Bootcamp.Database.dll in $PWD or its subfolders"
+}
+
+Write-Host "Executing dotnet $databaseAssembly $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser <REDACTED>"
+dotnet $databaseAssembly $DatabaseAction $DatabaseServer $DatabaseName $scriptDir $DatabaseUser $DatabasePassword
 if ($lastexitcode -ne 0) {
     throw ("Database migration had an error.")
 }
