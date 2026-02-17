792f64d564c3efa8242c7feb3deefb0e845fd5a0
Sean Spaniel <sean.spaniel@clear-measure.com>
2026-02-17 09:10:39 -0500

    registered app insights and sql server with aspire

--- src/ChurchBulletin.AppHost/AppHost.cs
+++ src/ChurchBulletin.AppHost/AppHost.cs
@@ -1,5 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.UI_Server>("ui-server");
+var sql = builder.AddConnectionString("SqlConnectionString");
+var appInsights = builder.AddConnectionString("AppInsights");
+
+builder.AddProject<Projects.UI_Server>("ui-server")
+    .WithReference(sql)
+    .WithReference(appInsights)
+    .WithEnvironment("AI_OpenAI_ApiKey", builder.Configuration["AI_OpenAI_ApiKey"])
+    .WithEnvironment("AI_OpenAI_Url", builder.Configuration["AI_OpenAI_Url"])
+    .WithEnvironment("AI_OpenAI_Model", builder.Configuration["AI_OpenAI_Model"]);
 
 builder.Build().Run();
