da6746738704499f1f1f4e2970ce8685c66c16bf
Casey Klaips <Casey.Klaips@biberk.com>
2026-03-02 11:03:04 -0600

    ck - push changes, still need to change assign to shelve

---
+++ src/Core/Model/StateCommands/InProgressToAssignedCommand.cs
@@ -0,0 +1,33 @@
+using ClearMeasure.Bootcamp.Core.Services;
+
+namespace ClearMeasure.Bootcamp.Core.Model.StateCommands;
+
+public record InProgressToAssignedCommand(WorkOrder WorkOrder, Employee CurrentUser) : StateCommandBase(WorkOrder,
+CurrentUser)
+{
+    public const string Name = "Assign";
+    public override string TransitionVerbPresentTense => Name;
+
+    public override string TransitionVerbPastTense => "Assigned";
+
+    public override WorkOrderStatus GetBeginStatus()
+    {
+        return WorkOrderStatus.InProgress;
+    }
+
+    public override WorkOrderStatus GetEndStatus()
+    {
+        return WorkOrderStatus.Assigned;
+    }
+
+    protected override bool UserCanExecute(Employee currentUser)
+    {
+        return currentUser == WorkOrder.Assignee;
+    }
+
+    public override void Execute(StateCommandContext context)
+    {
+        WorkOrder.CompletedDate = null;
+        base.Execute(context);
+    }
+}
