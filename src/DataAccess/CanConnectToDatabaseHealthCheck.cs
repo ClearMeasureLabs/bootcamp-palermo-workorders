482cabad3a92c792e99bc3218b4fba5dcf5c106e
Jeffrey Palermo <jeffrey@clear-measure.com>
2026-03-02 01:16:13 -0600

    fixed warning

--- src/DataAccess/CanConnectToDatabaseHealthCheck.cs
+++ src/DataAccess/CanConnectToDatabaseHealthCheck.cs
@@ -1,15 +1,16 @@
 ﻿using ClearMeasure.Bootcamp.DataAccess.Mappings;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 
 namespace ClearMeasure.Bootcamp.DataAccess;
 
-public class CanConnectToDatabaseHealthCheck(ILogger<CanConnectToDatabaseHealthCheck> logger, DataContext context)
+public class CanConnectToDatabaseHealthCheck()
     : IHealthCheck
 {
-    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context1,
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context1,
         CancellationToken cancellationToken = new())
     {
-        return Task.FromResult(HealthCheckResult.Healthy());
+        return new HealthCheckResult(HealthStatus.Healthy);
     }
 }
