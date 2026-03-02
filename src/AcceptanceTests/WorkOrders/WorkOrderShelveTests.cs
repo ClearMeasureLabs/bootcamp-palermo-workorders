2c09b32cbfaf80ee35be258bdb8e38af3a2c167f
Hunter Daley <hunter.daley@biberk.com>
2026-03-02 16:35:31 -0500

    work order shelve tests

---
+++ src/AcceptanceTests/WorkOrders/WorkOrderShelveTests.cs
@@ -0,0 +1,34 @@
+using ClearMeasure.Bootcamp.Core.Queries;
+using ClearMeasure.Bootcamp.UI.Shared.Pages;
+
+namespace ClearMeasure.Bootcamp.AcceptanceTests.WorkOrders;
+
+public class WorkOrderShelveTests : AcceptanceTestBase
+{
+    [Test, Retry(2)]
+    public async Task ShouldShelveWorkOrder()
+    {
+        await LoginAsCurrentUser();
+
+        var order = await CreateAndSaveNewWorkOrder();
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+        order = await AssignExistingWorkOrder(order, CurrentUser.UserName);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        order = await BeginExistingWorkOrder(order);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+        
+        order = await ShelveExistingWorkOrder(order);
+        order = await ClickWorkOrderNumberFromSearchPage(order);
+
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        await Click(nameof(WorkOrderSearch.Elements.WorkOrderLink) + order.Number);
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+        var rehyratedOrder = await Bus.Send(new WorkOrderByNumberQuery(order.Number!)) ??
+            throw new InvalidOperationException();
+
+        rehyratedOrder.Status.ShouldBe(WorkOrderStatus.Assigned);
+    }
+
+}
