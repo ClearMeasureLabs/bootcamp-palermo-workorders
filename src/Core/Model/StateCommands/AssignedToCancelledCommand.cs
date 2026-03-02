03019ba6acecedcbba5b2d340ed90e894dbfe092
Paige Ludecker <Paige.Ludecker@biberk.com>
2026-03-02 14:37:19 -0500

    Add cancelled

---
+++ src/Core/Model/StateCommands/AssignedToCancelledCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearMeasure.Bootcamp.Core.Model.StateCommands;
+
+public record AssignedToCancelledCommand(WorkOrder WorkOrder, Employee CurrentUser)
+: StateCommandBase(WorkOrder, CurrentUser)
+{
+    public const string Name = "Cancel";
+
+    public override WorkOrderStatus GetBeginStatus()
+    {
+        return WorkOrderStatus.Assigned;
+    }
+
+    public override WorkOrderStatus GetEndStatus()
+    {
+        return WorkOrderStatus.Cancelled;
+    }
+
+    protected override bool UserCanExecute(Employee currentUser)
+    {
+        return currentUser == WorkOrder.Creator;
+    }
+
+    public override string TransitionVerbPresentTense => Name;
+
+    public override string TransitionVerbPastTense => "Cancelled";
+}
