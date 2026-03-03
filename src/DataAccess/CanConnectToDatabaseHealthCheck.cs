adf27bf280b0b46bbb08889037488cc2e7d02f08
jnlarsen <jnlarsen@outlook.com>
2026-03-02 21:47:18 -0600

    Implemented CanConnectToDatabaseHealthCheck

--- src/DataAccess/CanConnectToDatabaseHealthCheck.cs
+++ src/DataAccess/CanConnectToDatabaseHealthCheck.cs
@@ -5,12 +5,22 @@ using Microsoft.Extensions.Logging;
 
 namespace ClearMeasure.Bootcamp.DataAccess;
 
-public class CanConnectToDatabaseHealthCheck()
+public class CanConnectToDatabaseHealthCheck(DbContext context, ILogger<CanConnectToDatabaseHealthCheck> logger)
     : IHealthCheck
 {
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context1,
         CancellationToken cancellationToken = new())
     {
+        try
+        {
+            await context.Database.CanConnectAsync(cancellationToken);
+            logger.LogDebug("Health check success via DbContext");
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarning(ex, "Database connection failed");
+            return new HealthCheckResult(HealthStatus.Unhealthy, exception: ex);
+        }
         return new HealthCheckResult(HealthStatus.Healthy);
     }
 }
