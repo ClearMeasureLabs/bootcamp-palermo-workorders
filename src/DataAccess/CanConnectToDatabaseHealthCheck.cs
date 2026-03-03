291885b85097588a85a6830e40d126721eb5c95f
Cole Sullivan <cole.sullivan@biberk.com>
2026-03-02 22:38:09 -0600

    Add DB health check

--- src/DataAccess/CanConnectToDatabaseHealthCheck.cs
+++ src/DataAccess/CanConnectToDatabaseHealthCheck.cs
@@ -5,12 +5,23 @@ using Microsoft.Extensions.Logging;
 
 namespace ClearMeasure.Bootcamp.DataAccess;
 
-public class CanConnectToDatabaseHealthCheck()
+public class CanConnectToDatabaseHealthCheck(DbContext dbContext,
+    ILogger<CanConnectToDatabaseHealthCheck> logger)
     : IHealthCheck
 {
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context1,
         CancellationToken cancellationToken = new())
     {
-        return new HealthCheckResult(HealthStatus.Healthy);
+        try
+        {
+            await dbContext.Database.CanConnectAsync(cancellationToken);
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception ex)
+        {
+            var message = $"DB connection failed: {ex.Message}";
+            logger.LogWarning(message);
+            return HealthCheckResult.Unhealthy(message);
+        }
     }
 }
