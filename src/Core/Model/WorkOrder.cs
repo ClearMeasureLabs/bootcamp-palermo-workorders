987413cba6fdc9db39347121a5ddee8209a1f4e4
Paige Ludecker <Paige.Ludecker@biberk.com>
2026-03-04 10:56:45 -0600

    Use ToUpperInvariant() and truncate before uppercasing per review feedback

--- src/Core/Model/WorkOrder.cs
+++ src/Core/Model/WorkOrder.cs
@@ -38,9 +38,17 @@ public class WorkOrder : EntityBase<WorkOrder>
             return string.Empty;
         }
 
-        var upperValue = value.ToUpper();
-        var maxLength = Math.Min(4000, upperValue.Length);
-        return upperValue.Substring(0, maxLength);
+        const int maxLength = 4000;
+
+        var truncated = value.Length > maxLength
+            ? value.Substring(0, maxLength)
+            : value;
+
+        var upperValue = truncated.ToUpperInvariant();
+
+        return upperValue.Length > maxLength
+            ? upperValue.Substring(0, maxLength)
+            : upperValue;
     }
 
     protected string getTextForStatus()
