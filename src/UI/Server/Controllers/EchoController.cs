5e1038eeeb16d2114667d11159f133c9f8801d83
IBM Bob Agent <ibmbob@clearmeasure.com>
2026-07-22 23:41:09 +0000

    fix: add versioned route to EchoController (#7157) [AB#7157]

--- src/UI/Server/Controllers/EchoController.cs
+++ src/UI/Server/Controllers/EchoController.cs
@@ -6,6 +6,7 @@ namespace ClearMeasure.Bootcamp.UI.Server.Controllers;
 [ApiController]
 [ApiVersion("1.0")]
 [Route("api/[controller]")]
+[Route("api/v{version:apiVersion}/[controller]")]
 public class EchoController : ControllerBase
 {
     [HttpGet]
