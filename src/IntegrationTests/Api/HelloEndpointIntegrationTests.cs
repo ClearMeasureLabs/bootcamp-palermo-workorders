fea9f486511f391500438bd092661e7e84e36285
IBM Bob Agent <ibmbob@clearmeasure.com>
2026-07-30 00:12:14 +0000

    Add /api/hello endpoint returning greeting #8217 [AB#8217]

---
+++ src/IntegrationTests/Api/HelloEndpointIntegrationTests.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using System.Net.Http.Json;
+using Shouldly;
+
+namespace ClearMeasure.Bootcamp.IntegrationTests.Api;
+
+[TestFixture]
+public class HelloEndpointIntegrationTests
+{
+    private DiagnosticsWebApplicationFactory? _factory;
+    private HttpClient? _client;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _factory = new DiagnosticsWebApplicationFactory();
+        _client = _factory.CreateClient();
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        _client?.Dispose();
+        _factory?.Dispose();
+    }
+
+    [Test]
+    public async Task Should_Return200AndJsonGreeting_When_GetHello()
+    {
+        var response = await _client!.GetAsync("/api/hello");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        var mediaType = response.Content.Headers.ContentType?.MediaType;
+        mediaType.ShouldNotBeNull();
+        mediaType!.ShouldContain("application/json");
+        
+        var result = await response.Content.ReadFromJsonAsync<HelloResponse>();
+        result.ShouldNotBeNull();
+        result!.Message.ShouldBe("Hello, World!");
+    }
+
+    private record HelloResponse(string Message);
+}
