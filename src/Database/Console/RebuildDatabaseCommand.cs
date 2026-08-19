cbe3ffe761cb53d59f0fe5b0aa7555dffc5657f8
Jeffrey Palermo <jeffrey@clear-measure.com>
2026-08-19 16:07:32 -0500

    Reduce Database console CRAP scores to 15 or below (#8638)
    Extract connection-string building, script baseline marking, and rebuild steps into focused helper classes. Add unit tests for validation and connection-string behavior.

--- src/Database/Console/RebuildDatabaseCommand.cs
+++ src/Database/Console/RebuildDatabaseCommand.cs
@@ -1,8 +1,4 @@
-﻿using DbUp;
-using DbUp.Engine;
-using DbUp.Helpers;
-using DbUp.Support;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
@@ -18,48 +14,22 @@ public class RebuildDatabaseCommand() : AbstractDatabaseCommand("Rebuild")
     {
         var scriptDir = GetScriptDirectory(options);
 
-        // 1) RunOnce scripts: Create + Update (journaled)
-        var createAndUpdateEngine = DeployChanges.To
-            .SqlDatabase(connectionString)
-            .WithScriptsFromFileSystem(Path.Join(scriptDir, "Create"))
-            .WithScriptsFromFileSystem(Path.Join(scriptDir, "Update"))
-            .JournalToSqlTable("dbo", "SchemaVersions")
-            .LogTo(new QuietLog())
-            .Build();
-
-        var createAndUpdateResult = createAndUpdateEngine.PerformUpgrade();
+        var createAndUpdateResult = DatabaseRebuildSteps.RunCreateAndUpdate(connectionString, scriptDir);
         if (!createAndUpdateResult.Successful)
         {
-            return Fail(createAndUpdateResult.Error?.ToString() ?? "Could not run scripts to rebuild database.");
+            return Fail(createAndUpdateResult.ErrorMessage ?? "Could not run scripts to rebuild database.");
         }
 
-        // 2) RunAlways scripts: things to re-apply each run (procs/views/perms)
-        var everytimeEngine = DeployChanges.To
-            .SqlDatabase(connectionString)
-            .WithScriptsFromFileSystem(Path.Join(scriptDir, "Everytime"),
-                new SqlScriptOptions { ScriptType = ScriptType.RunAlways })
-            .JournalTo(new NullJournal())
-            .LogTo(new QuietLog())
-            .Build();
-
-        var everytimeResult = everytimeEngine.PerformUpgrade();
+        var everytimeResult = DatabaseRebuildSteps.RunEverytime(connectionString, scriptDir);
         if (!everytimeResult.Successful)
         {
-            return Fail(everytimeResult.Error?.ToString() ?? "Failed to re-apply RunAlways scripts.");
+            return Fail(everytimeResult.ErrorMessage ?? "Failed to re-apply RunAlways scripts.");
         }
 
-        // 3) Optional test data pass (journaled or not, your choice)
-        var testDataEngine = DeployChanges.To
-            .SqlDatabase(connectionString)
-            .WithScriptsFromFileSystem(Path.Join(scriptDir, "TestData"))
-            .JournalToSqlTable("dbo", "SchemaVersions")
-            .LogTo(new QuietLog())
-            .Build();
-
-        var testDataResult = testDataEngine.PerformUpgrade();
+        var testDataResult = DatabaseRebuildSteps.RunTestData(connectionString, scriptDir);
         if (!testDataResult.Successful)
         {
-            return Fail(testDataResult.Error?.ToString() ?? "Failed to run TestData scripts.");
+            return Fail(testDataResult.ErrorMessage ?? "Failed to run TestData scripts.");
         }
 
         AnsiConsole.MarkupLine($"[green]Finished updating {options.DatabaseName}.[/]");
