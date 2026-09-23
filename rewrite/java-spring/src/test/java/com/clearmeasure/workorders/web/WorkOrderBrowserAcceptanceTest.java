2b71ee490cc7881ab7fa49dbc88f384093523894
Jeffrey Palermo <jeffrey@clear-measure.com>
2026-09-23 08:45:32 -0500

    Import Playwright wait state for acceptance test

--- rewrite/java-spring/src/test/java/com/clearmeasure/workorders/web/WorkOrderBrowserAcceptanceTest.java
+++ rewrite/java-spring/src/test/java/com/clearmeasure/workorders/web/WorkOrderBrowserAcceptanceTest.java
@@ -2,6 +2,7 @@ package com.clearmeasure.workorders.web;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
